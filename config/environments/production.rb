208482da1b4b4d6d3e6844568552dad52cced5d8
GiuseppeXD <giuseppelucam@hotmail.com>
2021-02-01 01:16:05 -0300

    heroku test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.hosts << "limitless-stream-35993.herokuapp.com"
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
